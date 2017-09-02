3e8014196a71039833a43f05d1186e9ae6aeef70
Fritz Gerald  Santos <fritzgeraldsantos@Fritzs-MacBook-Pro.local>
2017-09-03 00:52:11 +0100

    Stack

---
+++ src/mydatastructures/StackInterface.java
@@ -0,0 +1,41 @@
+package mydatastructures;
+
+public interface StackInterface<T> {
+	
+	/**
+	 * 
+	 * @return true if stack is empty,
+	 * else returns false if stack contains elements
+	 */
+	public boolean isEmpty();
+	
+	
+	/**
+	 * 
+	 * @return element at the top of stack but does not remove it.
+	 */
+	public T peek();
+	
+	/**
+	 * 
+	 * @return element at the top of the stack and removes it from stack.
+	 */
+	public T pop();
+	
+	/**
+	 * adds element to stack
+	 * @param element
+	 * @return element
+	 */
+	public T push(T element);
+	
+	/**
+	 * searches for element in stack, if found, it's index from the top of stack is returned, otherwise, 1 is returned.
+	 * @param element
+	 * @return element
+	 */
+	public int search(T element);
+	
+	
+
+}
---
+++ test/datastructures/StackTest.java
@@ -0,0 +1,70 @@
+package datastructures;
+
+import mydatastructures.Stack;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class StackTest {
+	
+	private Stack<Integer> stack = new Stack();
+
+
+
+	int test1 = 10;
+	int test2 = 2;
+	int test3 = 124;
+	
+	@Test
+	public void testPush() {
+		stack.push(test1);
+		stack.push(test2);
+		stack.push(test3);
+		stack.push(test3);
+		stack.push(test3);
+		stack.push(test3);
+		stack.push(test3);
+		stack.push(test3);
+		stack.push(test3);
+		stack.push(test3);
+		stack.push(8353);
+		stack.push(12412);
+		stack.push(325234);
+	}
+	
+	@Test
+	public void testPop()
+	{
+		testPush();
+		System.out.println(stack.peek());
+		stack.pop();
+		System.out.println(stack.peek());
+	}
+	
+	@Test
+	public void testIsEmpty()
+	{
+		testPush();
+
+		System.out.println("Size: " + stack.size());
+		if(stack.isEmpty())
+		{
+			System.out.println("Empty");
+		}
+		else
+		{
+			System.out.println("Not Empty");
+		}
+	}
+	
+	@Test
+	public void testSearch()
+	{
+		testPush();
+		System.out.println(stack.search(2));
+	}
+
+}
